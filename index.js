0e39f044a185f1511c6a03ae7bda743d4e0427da
Goooble <bivinbheemaiah@gmail.com>
2026-01-26 18:26:36 +0530

    feat: Change color if char is correct
    user entry shows up on top of the sample text. If
    correct, it shows up as green and if not, it shows
    up as red.

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ process.title = "Typing Test";
 //state
 const cols = process.stdout.columns;
 const rows = process.stdout.rows; //wonder what happens when rows exceed the screen
-const cursor = { x: 0, y: 0 };
-const user = "";
+const cursor = { x: 0, y: 2 };
+let user = "";
+let userDisplay = [];
+let index = 0;
+let sample =
+  "I think that by the time I turn sixty, I will probably be surrounded by children and grandchildren. I have visions of sitting out on a large verandah overlooking the immense garden. The garden would be a riot of colors, most of the plants having been planted by me.".toLowerCase();
 
 //stream handling
 const wstream = process.stdout;
@@ -17,19 +21,29 @@ rstream.resume();
 //key press
 rstream.on("data", (chunk) => {
   const key = handleInput(chunk);
-  console.log(chunk);
-  //   render();
+  render();
 });
 
 render();
+wstream.cursorTo(cursor.x, cursor.y);
 
 function render() {
   console.clear();
   console.log(process.title);
   console.log("Sample text:");
-  console.log(
-    "I think that by the time I turn sixty, I will probably be surrounded by children and grandchildren. I have visions of sitting out on a large verandah overlooking the immense garden. The garden would be a riot of colors, most of the plants having been planted by me.",
-  );
+  console.log(sample);
+  //   console.log(userDisplay);
+  cursor.x = 0;
+  cursor.y = 2;
+  for (let i = 0; i < userDisplay.length; i++) {
+    wstream.cursorTo(cursor.x, cursor.y);
+    wstream.write(userDisplay[i]);
+    cursor.x++;
+    if (cursor.x == cols) {
+      cursor.x = 0;
+      cursor.y++;
+    }
+  }
 }
 
 function handleInput(chunk) {
@@ -37,12 +51,27 @@ function handleInput(chunk) {
   if (chunk.length == 1) {
     char = chunk[0];
   }
-  if ((char >= 97 && char <= 122) || (char >= 65 && char <= 90)) {
-    // type(char);
+  if (char >= 32 && char <= 126) {
+    // console.log(char);
+    type(char);
   } else if (char == 0x7f) {
     // backspace();
     // console.log("bac");
   } else if (char == 0x1b) {
     // do something
   }
+}
+
+function type(char) {
+  //   console.log(char);
+  char = String.fromCharCode(char);
+  user += char;
+  if (sample[index] == char) {
+    userDisplay.push("\x1b[32m" + char + "\x1b[0m");
+    // console.log(userDisplay);
+  } else {
+    userDisplay.push("\x1b[31m" + char + "\x1b[0m");
+    // process.abort();
+  }
+  index++;
 }
